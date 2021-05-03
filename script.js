b2c6f0584b1891f8c0277ef16ee816bcd65727a9
dhruvi-mehta <mehtd083@newschool.edu>
2021-05-02 22:47:23 -0400

    hi

--- script.js
+++ script.js
@@ -1,104 +1,30 @@
 console.log("hello");
 
-
- var cnt = 0;
-
-  function showl1() {
-    // document.body.replace("index.html", '_blank');
-}
-
-  // function showl2 () {
-  //   var layer2 = document.querySelector('.item2');
-  //   layer2.addEventListener("click", function(){
-
-  //       if (layer2.classList.contains("item2")){
-  //           layer2.style.display = "block";
-  //     } else {
-  //       layer2.style.display = "none";
-  //     }
-  //    });
-
-  // }
-
-  // function showl3() {
-
-  // }
-
-  // function showl4() {
-
-  // }
-
-//     html2canvas(document.querySelector("#box")).then(canvas => {
-//         document.body.appendChild(canvas)
-
-//         this.href = document.getElementById("box").toDataURL();
-//         this.download = "export.png";
-//     });
-
-
-// var filterFamily = document.querySelector('#family');
-//     filterFamily.addEventListener("click", function(){
-
-//         if (movieContainer.classList.contains("family")){
-//             movieContainer.style.display = "block";
-//       } else {
-//         movieContainer.style.display = "none";
-//       }
-//      });
-
-
-
-
-function pattern(prefix, max_suffix) {
-    if (window.cnt == 0) {
-      document.getElementById("first-image").remove();
+document.querySelector('.motif').addEventListener('click', (event) => {
+    if (event.target.tagName === 'IMG') {
+        if (cnt === 0) document.getElementById('first-image').remove();
+        if (cnt >= 4) {
+            alert('You can only have a maximum of 4 patterns on your canvas.Try to start again!');
+            window.location.reload();
+        }
+ 
+        const img = document.createElement('img');
+        img.src = event.target.src;
+        img.style.zIndex = cnt;
+        img.style.position = absolute;
+        img.style.top = '0';
+        img.style.left = '0';
+ 
+        const appendToBox = () => {
+            document.getElementById('box').appendChild(img);
+        };
+ 
+        if ('decoding' in img) {
+            img.decode().then(appendToBox);
+        } else {
+            img.onload = appendToBox;
+        }
+ 
+        cnt++;
     }
-    if (window.cnt >= 4) {
-      alert("You can only have a maximum of 4 patterns on your canvas.Try to start again!");
-      document.body.replace("index.html", '_blank');
-
-    } else {
-      var suffix = Math.floor( Math.random() * (max_suffix - 1) + 2 );
-      var img = document.createElement("img");
-      img.id = "canvas";
-      img.src = "images/" + prefix + "-" + suffix.toString().padStart(2, '0') + ".svg";
-      img.style = "z-index: " + window.cnt + "; position: absolute; top: 0; left: 0;"
-      var img_div_position = document.getElementById("box");
-      img_div_position.appendChild(img);
-      window.cnt = window.cnt + 1;
-    }
-  }
-
-
-// $(function(){
-//   function img(prefix, max_suffix) {
-//     var suffix = Math.floor( Math.random() * (max_suffix - 1) + 1 );
-//     var img = document.createElement("img");
-//     img.src = prefix + '-' + suffix;
-//     img.style = "z-index: {cnt}; position: absolute; top: 0; left: 0;"
-//     var img_div_position = document.getElementById("box");
-//     img_div_position.appendChild(img);
-//   }
-
-// });
-
-
-// let canvas = document.querySelector("#canvas");
-
-// canvas.addEventListener("onclick", function (event) {
-//   console.log("This is the onclick event");
-//   console.log(event);
-  
-
-
-// $(function(){
-//   function call_img(prefix, max_suffix) {
-//     var suffix = Math.floor( Math.random() * (max_suffix - 1) + 1 );
-//     var img = document.createElement("img");
-//     img.src = prefix + '-' + suffix;
-//     img.style = "z-index: {cnt}; position: absolute; top: 0; left: 0;"
-//     var img_div_position = document.getElementById("box");
-//     img_div_position.appendChild(img);
-//   }
-
-// });
+});
