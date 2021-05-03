f78dd5553ff08751d2b020f7950fb2a2df51e2d6
dhruvi-mehta <mehtd083@newschool.edu>
2021-05-03 00:45:45 -0400

    hi

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log("hello");
 
 let cnt = 0;
-
+ 
 document.querySelector('.motif').addEventListener('click', (event) => {
     if (event.target.tagName === 'IMG') {
         if (cnt === 0) document.getElementById('first-image').remove();
@@ -13,7 +13,7 @@ document.querySelector('.motif').addEventListener('click', (event) => {
         const img = document.createElement('img');
         img.src = event.target.src;
         img.style.zIndex = cnt;
-        img.style.position = absolute;
+        img.style.position = 'absolute';
         img.style.top = '0';
         img.style.left = '0';
  
